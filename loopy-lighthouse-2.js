9f4dc94f55dcf1c4edfa41a03bd38e48049aed62
justinkwanchan <justinkwanchan@gmail.com>
2020-11-24 00:11:47 +0000

    Refactored loopy-lighthouse-2.js

--- loopy-lighthouse-2.js
+++ loopy-lighthouse-2.js
@@ -1,15 +1,17 @@
 const loopyLighthouse = (range, multiples, words) => {
+  // Iterates over entire range of number provided, inclusive
   for (let i = range[0]; i <= range[1]; i++) {
-    if (i % multiples[0] === 0 && i % multiples[1] === 0) {
-      console.log(words[0] + words[1]);
-    } else if (i % multiples[0] === 0) {
-      console.log(words[0]);
-    } else if (i % multiples[1] === 0) {
-      console.log(words[1]);
-    } else {
-      console.log(i);
+    let answer = '';
+    if (i % multiples[0] === 0) {
+      answer += words[0];
     }
+    if (i % multiples[1] === 0) {
+      answer += words [1];
+    }
+    console.log(answer === '' ? i : answer);
+    answer = '';
   }
-}
+};
 
 loopyLighthouse([15, 90], [2, 5], ["Batty", "Beacon"]);
+loopyLighthouse([1, 40], [3, 4], ["Jackie", "Chan"]);
